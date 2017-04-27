3fd69179f4461be113081da816b5c3b8f0919ee3
Joe Gaudet <joe.gaudet@food.ee>
2017-04-27 11:58:37 -0700

    Add a bunch of table steaks stuff without tests

---
+++ addon/components/errors-for.js
@@ -0,0 +1,32 @@
+import Ember from 'ember';
+import layout from '../templates/components/errors-for';
+
+
+const {
+  computed,
+  get,
+} = Ember;
+
+/**
+ * This class conditionally renders an array of errors
+ * @class ErrorsFor
+ */
+export default Ember.Component.extend({
+  layout,
+
+  config: computed(function () {
+    return get(Ember.getOwner(this).resolveRegistration('config:environment'), 'APP.ember-form-for');
+  }),
+
+  // remove tags, so we don't interfere with styles that use direct inheritance
+  tagName: '',
+
+  /**
+   * Errors to be rendered by this component
+   * @property label
+   * @type String
+   * @default null
+   * @public
+   */
+  errors: null
+});
--- addon/components/form-button.js
+++ addon/components/form-button.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import layout from '../templates/components/commit-buttons';
+import layout from '../templates/components/form-button';
 
 const {
   computed,
@@ -9,18 +9,9 @@ const {
 export default Ember.Component.extend({
   layout,
 
-  // remove tags, so we don't interfere with styles that use direct inheritance
-  tagName: '',
-
   config: computed(function () {
     return get(Ember.getOwner(this).resolveRegistration('config:environment'), 'APP.ember-form-for');
   }),
 
-  visible: false,
-
-  commit(){
-  },
-
-  cancel(){
-  }
+  tagName: ''
 });
