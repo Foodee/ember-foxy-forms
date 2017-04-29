26cf37be9157771722d39de21cf6890481855a58
Kenneth Buck <kenneth@nestedsphere.com>
2017-04-28 22:37:14 -0400

    Put obligatory -test post on field-for test module. Clear out default test

---
+++ tests/integration/components/field-for-test.js
@@ -0,0 +1,10 @@
+import { moduleForComponent, test } from 'ember-qunit';
+// import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('field-for', 'Integration | Component | field container', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  assert.expect(0);
+});
