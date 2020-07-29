f6050b2af4b6f9f1a19f60f942d827bf6f49d789
Joe Gaudet <joe.gaudet@food.ee>
2020-07-29 10:54:45 -0700

    Add model to did submit hook

--- addon/components/form-for.js
+++ addon/components/form-for.js
@@ -688,7 +688,7 @@ export default class FormForComponent extends Component {
         })
         .then(() => {
           this.notifySuccess(this.args.successfulSubmitMessage);
-          this.didSubmit();
+          this.didSubmit(model);
           this.notifyChildDidSubmit(false);
           this._markClean();
           this._runFieldDidSubmit();
