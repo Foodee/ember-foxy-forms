7c01d41d8151662daa4cf7ee926288a7d9158d9b
Joe Gaudet <joe.gaudet@food.ee>
2020-08-15 15:09:45 -0700

    Make isActing tracked

--- addon/components/form-button.js
+++ addon/components/form-button.js
@@ -1,7 +1,9 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class FormButtonComponent extends Component {
+  @tracked
   isActing = false;
   @action
   handleClick() {
