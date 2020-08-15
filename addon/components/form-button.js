f74c35cbf75fa356109bdf2a720bd93f8713a575
Joe Gaudet <joe.gaudet@food.ee>
2020-08-15 14:14:57 -0700

    Fix is acting guard

--- addon/components/form-button.js
+++ addon/components/form-button.js
@@ -5,7 +5,7 @@ export default class FormButtonComponent extends Component {
   isActing = false;
   @action
   handleClick() {
-    if (!this.args.isActing && this.args.onClick) {
+    if (!this.isActing && this.args.onClick) {
       const ret = this.args.onClick();
 
       if (ret.finally) {
