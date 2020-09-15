c92091fe9464df5094fed63e20334389ac5319d8
Joe Gaudet <joe.gaudet@food.ee>
2020-09-14 21:22:48 -0700

    Allow for inversion of checkboxes
    This commit lets checkboxes be inverted, there are times where you might
    want checking a checkbox to have the opposite effect on the underlying
    model. Which is to say render selected when its value is absent.

--- addon/components/form-controls/ff-checkbox.js
+++ addon/components/form-controls/ff-checkbox.js
@@ -1,10 +1,19 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { guidFor } from '@ember/object/internals';
+import { arg } from 'ember-arg-types';
+import { bool } from 'prop-types';
 
 export default class FormControlsFfCheckboxComponent extends Component {
+  @arg value = false;
+  @arg(bool) isInverted = false;
+
   get id() {
     return guidFor(this);
+  }
+
+  get checked() {
+    return this.isInverted ? !this.value : this.value;
   }
 
   @action
