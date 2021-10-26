e173f878065f740e4536571daeb0d1b67c0f66fa
Arthur Goldsmith <arthur5005@gmail.com>
2021-10-26 11:01:06 -0700

    Fix issue with use of union helper in template
    * Fixes an issue that prevents us from using the union ember-composable-helper with a relationship proxy array from Ember
    * Uses a computed property to add null to the beginning of an array for when nones are allowed

--- addon/components/form-controls/ff-radio.js
+++ addon/components/form-controls/ff-radio.js
@@ -1,3 +1,7 @@
 import FormControlsFfSelectComponent from './ff-select';
 
-export default class FormControlsFfRadioComponent extends FormControlsFfSelectComponent {}
+export default class FormControlsFfRadioComponent extends FormControlsFfSelectComponent {
+  get valuesWithNone() {
+    return this.allowNone ? [null, ...this.values.toArray()] : this.values;
+  }
+}
