b61477c861883e214a3777090a0de82e49f07f23
Joe Gaudet <joe@joegaudet.com>
2022-04-07 09:51:22 -0700

    Add max length display to text area

--- addon/components/form-controls/ff-textarea.js
+++ addon/components/form-controls/ff-textarea.js
@@ -1,3 +1,34 @@
 import FormControlsFfInputComponent from './ff-input';
+import { arg } from 'ember-arg-types';
+import { bool } from 'prop-types';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
-export default class FormControlsFfTextareaComponent extends FormControlsFfInputComponent {}
+export default class FormControlsFfTextareaComponent extends FormControlsFfInputComponent {
+
+  @tracked length;
+
+  @arg(bool) showMaxLength = false;
+
+  @arg maxLength;
+
+  constructor() {
+    super(...arguments);
+
+    this.length = this.args.value?.length ?? 0;
+  }
+
+  get _showMaxLength() {
+    return this.showMaxLength && this.maxLength;
+  }
+
+  @action
+  handleChange(event) {
+    this.length = event.target.value.length;
+
+    if (this.args.onChange) {
+      return this.args.onChange(event.target.value);
+    }
+  }
+
+}
